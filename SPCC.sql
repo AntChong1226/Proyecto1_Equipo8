8beaae0fed3c0cbff4a6941fc56e274b96e7d5fa
Diego260603 <65355590+Diego260603@users.noreply.github.com>
2020-05-31 00:00:11 -0500

    FEAT= DB added

---
+++ SPCC.sql
@@ -0,0 +1,275 @@
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: SPCC
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrador`
+--
+
+DROP TABLE IF EXISTS `administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrador` (
+  `id_tipoUs` int(4) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `contraseña` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoUs`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrador`
+--
+
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alimento`
+--
+
+DROP TABLE IF EXISTS `alimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alimento` (
+  `id_alimento` int(3) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_alimento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alimento`
+--
+
+LOCK TABLES `alimento` WRITE;
+/*!40000 ALTER TABLE `alimento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alimento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `colegio`
+--
+
+DROP TABLE IF EXISTS `colegio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `colegio` (
+  `id_colegio` int(2) NOT NULL,
+  `colegio` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_colegio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `colegio`
+--
+
+LOCK TABLES `colegio` WRITE;
+/*!40000 ALTER TABLE `colegio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `colegio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudiante`
+--
+
+DROP TABLE IF EXISTS `estudiante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estudiante` (
+  `id_tipoCliente` int(4) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `numCuenta` int(9) DEFAULT NULL,
+  `id_grupo` int(2) DEFAULT NULL,
+  `contraseña` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoCliente`),
+  KEY `id_grupo` (`id_grupo`),
+  CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudiante`
+--
+
+LOCK TABLES `estudiante` WRITE;
+/*!40000 ALTER TABLE `estudiante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estudiante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionario` (
+  `id_tipoCliente` int(3) NOT NULL,
+  `id_colegio` int(2) DEFAULT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `RFC` varchar(13) DEFAULT NULL,
+  `contraseña` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoCliente`),
+  KEY `id_colegio` (`id_colegio`),
+  CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`id_colegio`) REFERENCES `colegio` (`id_colegio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionario`
+--
+
+LOCK TABLES `funcionario` WRITE;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo`
+--
+
+DROP TABLE IF EXISTS `grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo` (
+  `id_grupo` int(2) NOT NULL,
+  `grupo` int(3) NOT NULL,
+  PRIMARY KEY (`id_grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo`
+--
+
+LOCK TABLES `grupo` WRITE;
+/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesor` (
+  `id_tipoCliente` int(3) NOT NULL,
+  `id_colegio` int(2) DEFAULT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `RFC` varchar(13) DEFAULT NULL,
+  `contraseña` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoCliente`),
+  KEY `id_colegio` (`id_colegio`),
+  CONSTRAINT `profesor_ibfk_1` FOREIGN KEY (`id_colegio`) REFERENCES `colegio` (`id_colegio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesor`
+--
+
+LOCK TABLES `profesor` WRITE;
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supervisor`
+--
+
+DROP TABLE IF EXISTS `supervisor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supervisor` (
+  `id_tipoUs` int(13) NOT NULL,
+  `contraseña` varchar(30) DEFAULT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoUs`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+LOCK TABLES `supervisor` WRITE;
+/*!40000 ALTER TABLE `supervisor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supervisor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trabajador`
+--
+
+DROP TABLE IF EXISTS `trabajador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trabajador` (
+  `id_tipoCliente` int(3) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `numTrabajador` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_tipoCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trabajador`
+--
+
+LOCK TABLES `trabajador` WRITE;
+/*!40000 ALTER TABLE `trabajador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trabajador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `zona_entrega`
+--
+
+DROP TABLE IF EXISTS `zona_entrega`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `zona_entrega` (
+  `id_zona` int(2) NOT NULL,
+  `zona` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_zona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zona_entrega`
+--
+
+LOCK TABLES `zona_entrega` WRITE;
+/*!40000 ALTER TABLE `zona_entrega` DISABLE KEYS */;
+/*!40000 ALTER TABLE `zona_entrega` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-30 22:46:01
